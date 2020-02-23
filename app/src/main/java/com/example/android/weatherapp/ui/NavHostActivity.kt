d194595ca64571f82efdd28f06adff40f5abc705
shrayanbajra <shrayanbajracharya@gmail.com>
2020-02-23 12:47:27 +0545

    Implemented code for item selection listener in Navigation Drawer

--- app/src/main/java/com/example/android/weatherapp/ui/NavHostActivity.kt
+++ app/src/main/java/com/example/android/weatherapp/ui/NavHostActivity.kt
@@ -1,12 +1,14 @@
 package com.example.android.weatherapp.ui
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import com.example.android.weatherapp.R
+import com.google.android.material.navigation.NavigationView
 
 class NavHostActivity : AppCompatActivity() {
 
@@ -27,6 +29,18 @@ class NavHostActivity : AppCompatActivity() {
             R.string.open_navigation_drawer,
             R.string.close_navigation_drawer
         )
+
+        val navigationView: NavigationView = findViewById(R.id.nav_view)
+        navigationView.setNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.nav_settings -> {
+                    Toast.makeText(applicationContext, "Settings Selected!", Toast.LENGTH_SHORT)
+                        .show()
+                    true
+                }
+                else -> false
+            }
+        }
 
         drawer.addDrawerListener(toggle)
         toggle.syncState()
