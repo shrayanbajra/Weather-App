e5a0e7e5eb39f30b3dbfa0c933ff2045dfca20b4
shrayanbajra <shrayanbajracharya@gmail.com>
2020-02-23 12:38:15 +0545

    Included code for drawer and toggle for navigation drawer

--- app/src/main/java/com/example/android/weatherapp/ui/NavHostActivity.kt
+++ app/src/main/java/com/example/android/weatherapp/ui/NavHostActivity.kt
@@ -1,13 +1,43 @@
 package com.example.android.weatherapp.ui
 
 import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import com.example.android.weatherapp.R
 
 class NavHostActivity : AppCompatActivity() {
 
+    private lateinit var drawer: DrawerLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_nav_host)
+
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        drawer = findViewById(R.id.drawer_layout)
+
+        val toggle = ActionBarDrawerToggle(
+            this,
+            drawer,
+            R.string.open_navigation_drawer,
+            R.string.close_navigation_drawer
+        )
+
+        drawer.addDrawerListener(toggle)
+        toggle.syncState()
+    }
+
+
+    override fun onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
     }
 }
