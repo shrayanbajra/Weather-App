7ca48367bd2f1dc66f84ecb06802d63d7e71086d
shrayanbajra <shrayanbajracharya@gmail.com>
2020-03-12 22:30:44 +0545

    Implemented listener in Settings Fragment distinguishing the selected preferences

--- app/src/main/java/com/example/android/weatherapp/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/example/android/weatherapp/ui/settings/SettingsFragment.kt
@@ -1,8 +1,7 @@
 package com.example.android.weatherapp.ui.settings
 
 import android.os.Bundle
-import androidx.preference.Preference
-import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.*
 import com.example.android.weatherapp.R
 
 class SettingsFragment : PreferenceFragmentCompat(),
@@ -10,11 +9,48 @@ class SettingsFragment : PreferenceFragmentCompat(),
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.app_preferences, rootKey)
+
+        bindSummaryValue(findPreference("pref_units"))
+        bindSummaryValue(findPreference("pref_location"))
     }
 
     override fun onPreferenceStartFragment(
         caller: PreferenceFragmentCompat?, pref: Preference?
     ): Boolean {
         return true
+    }
+
+    companion object {
+        fun bindSummaryValue(preference: Preference?) {
+            preference?.let {
+                preference.onPreferenceChangeListener = preferenceChangeListener
+                preferenceChangeListener.onPreferenceChange(
+                    preference,
+                    PreferenceManager.getDefaultSharedPreferences(preference.context)
+                        .getString(preference.key, "")
+                )
+            }
+        }
+
+        private val preferenceChangeListener: Preference.OnPreferenceChangeListener =
+            Preference.OnPreferenceChangeListener { preference, newValue ->
+                val selectedValue = newValue.toString()
+
+                when (preference) {
+                    is ListPreference -> {
+                        val listPreference: ListPreference = preference
+                        val index: Int = listPreference.findIndexOfValue(selectedValue)
+                        preference.summary = if (index > 0) {
+                            listPreference.entries[index]
+                        } else {
+                            ""
+                        }
+                    }
+                    is EditTextPreference -> {
+                        preference.setSummary(selectedValue)
+                    }
+                }
+                true
+            }
     }
 }
